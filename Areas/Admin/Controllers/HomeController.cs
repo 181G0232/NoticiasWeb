6cd9925e5cd3341bb28b6383103cc4989c451ef3
Juan Jesús Chávez Villa <janitochvvll2311@gmail.com>
2021-12-07 20:19:30 -0600

    Autenticacion y Autorizacion listos

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NoticiasWeb.Admin.Models.ViewModels;
@@ -9,6 +10,7 @@ namespace NoticiasWeb.Admin.Controllers
 {
 
     [Area("Admin")]
+    [Authorize]
     public class HomeController : Controller
     {
 
